c5bfbb588e951137155a40e22e238239038d8b2e
Eliza <itzjusteliza@gmail.com>
2020-04-09 21:43:51 +0200

    Lägger till Test mappen och test filen.

---
+++ test/returvarde.spec.js
@@ -0,0 +1,17 @@
+var assert = require('assert'),
+    { area } = require('../returvarde');
+
+
+it('Width and height produce the right value.', () => {
+    //Rätt
+    assert.strictEqual(area(3, 4), 84)
+})
+it('Parameter missing.', () => {
+    //Ger fel
+    assert.strictEqual(area(3), 84)
+})
+it('Width and height DO NOT produce the right value.', () => {
+    //Ger fel
+    assert.strictEqual(area(3, 5), 84)
+})
+
