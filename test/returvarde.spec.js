9a5e2e546681d9872f2da3d10f1fc23603f58ce0
Eliza <itzjusteliza@gmail.com>
2020-04-09 21:45:02 +0200

    Ändring i test filen. §

--- test/returvarde.spec.js
+++ test/returvarde.spec.js
@@ -6,12 +6,12 @@ it('Width and height produce the right value.', () => {
     //Rätt
     assert.strictEqual(area(3, 4), 84)
 })
-it('Parameter missing.', () => {
-    //Ger fel
-    assert.strictEqual(area(3), 84)
-})
-it('Width and height DO NOT produce the right value.', () => {
-    //Ger fel
-    assert.strictEqual(area(3, 5), 84)
-})
+// it('Parameter missing.', () => {
+//     //Ger fel
+//     assert.strictEqual(area(3), 84)
+// })
+// it('Width and height DO NOT produce the right value.', () => {
+//     //Ger fel
+//     assert.strictEqual(area(3, 5), 84)
+// })
 
